19e18ebefd8adf6fe33c07a1c770e0c8adc0b3ab
Omar Suárez Doro <91568642+OmarSuarezDoro@users.noreply.github.com>
2021-11-24 20:28:08 +0100

    Update 02-divisors-in-order.cc

--- IB-2021-2022-Practica08-Funciones-main/02-divisors-in-order/02-divisors-in-order.cc
+++ IB-2021-2022-Practica08-Funciones-main/02-divisors-in-order/02-divisors-in-order.cc
@@ -21,13 +21,13 @@ void MensajeInicial() {
 int CalculateDivisors(int number_introduced) {
   std::cout << "divisors of " << number_introduced << ":";
   for (int contador = 1; contador < sqrt(number_introduced); ++contador) {
-    if(number_introduced % contador == 0) {
+    if (number_introduced % contador == 0) {
       std::cout << " " << contador;
     } 
   }
   for (int contador = sqrt(number_introduced); contador >= 1; --contador) {
-    if(number_introduced % contador == 0) {
-      std::cout << " " <<(number_introduced / contador);
+    if (number_introduced % contador == 0) {
+      std::cout << " " << (number_introduced / contador);
     } 
 
   }
@@ -37,7 +37,7 @@ int CalculateDivisors(int number_introduced) {
 int main() {
   MensajeInicial();
   int number_introduced;
-  while(std::cin >> number_introduced) {
+  while (std::cin >> number_introduced) {
     CalculateDivisors(number_introduced);
     std::cout << "\n";
   }
